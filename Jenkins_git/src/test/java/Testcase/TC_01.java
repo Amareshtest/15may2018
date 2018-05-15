e455a651f00d888e44fd85109eebacb1edae2f47
Amareshtest <amaresh2014@gmail.com>
2018-05-15 06:19:04 +0530

    First commit

---
+++ Jenkins_git/src/test/java/Testcase/TC_01.java
@@ -0,0 +1,12 @@
+package Testcase;
+
+import org.testng.annotations.Test;
+
+public class TC_01 {
+	
+	@Test
+	public static void testcase_01() {
+		System.out.println("this is my first testcase");
+	}
+
+}
